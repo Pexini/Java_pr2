72b48c540b88b45d7d1e03478206aead86d2ea00
Davydov.P <davydov.works@gmail.com>
2023-07-05 13:26:40 -0400

    В 4 строке исправил Cost на cost

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        int coast = 13_686; // переменная для ввода стоимости билета.
+        int cost = 13_686; // переменная для ввода стоимости билета.
         int sale = 20; // кол-во бонусных мил за 1 руб.
-        int your_sale = coast / sale;
+        int your_sale = cost / sale;
 
         System.out.println("Вам начисленно : " + your_sale + " бонусных миль");
     }
