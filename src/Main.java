42c6350c2d0b8d679db872d0ed02f196798440ca
Davydov.P <davydov.works@gmail.com>
2023-07-04 20:43:44 -0400

    add cod

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        
 
+        int coast = 13_686; // переменная для ввода стоимости билета.
+        int sale = 20; // кол-во руб за 1 мил.
+        int your_sale = coast / sale;
 
+    System.out.println("Your miles per 1 trip : " + your_sale + " руб");
+        }
     }
-}
