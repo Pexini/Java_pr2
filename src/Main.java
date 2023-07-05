16d50b5e823ab0e7e7fcbc4107e4dcfb81c3f13b
Davydov.P <davydov.works@gmail.com>
2023-07-04 22:36:37 -0400

    Изменил строчку ответа программы

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ public class Main {
     public static void main(String[] args) {
 
         int coast = 13_686; // переменная для ввода стоимости билета.
-        int sale = 20; // кол-во руб за 1 мил.
+        int sale = 20; // кол-во бонусных мил за 1 руб.
         int your_sale = coast / sale;
 
-        System.out.println("Your miles per 1 trip : " + your_sale + " руб");
+        System.out.println("Вам начисленно : " + your_sale + " бонусных миль");
     }
 }
